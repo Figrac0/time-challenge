e3581dfa37faa3559eccd3c518fa86c70f14ae88
sergey <serjjiniuss@gmail.com>
2025-11-28 23:07:04 +0400

    style+

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ParticlesBackground from "./components/ParticlesBackground";
+import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
+
+function App() {
+    return (
+        <>
+            <ParticlesBackground />
+            <Player />
+            <div id="challenges">
+                <TimerChallenge title="Easy" targetTime={1} />
+                <TimerChallenge title="Not easy" targetTime={5} />
+                <TimerChallenge title="Getting tough" targetTime={10} />
+                <TimerChallenge title="Pros only" targetTime={15} />
+            </div>
+        </>
+    );
+}
+
+export default App;
